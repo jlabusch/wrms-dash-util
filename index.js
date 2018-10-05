fd691341119977a2bfbb17a329147c456efd5618
Jacques Labuschagne <jlabusch@acm.org>
2018-10-05 23:39:02 +0100

    Initial copypasta.

---
+++ index.js
@@ -0,0 +1,12 @@
+modules.exports = {
+    crypt:  require('./lib/crypt'),
+    dates:  require('./lib/dates'),
+    log:    require('./lib/log'),
+    map_severity:           require('./lib/map_severity'),
+    promise_sequence:       require('./lib/promise_sequence'),
+    round_to_half_hour:     require('./lib/round_to_half_hour'),
+    send_err_if_not_vendor: require('./lib/send_err_if_not_vendor'),
+    swapper:    require('./lib/swapper'),
+    trim:       require('./lib/trim')
+}
+
