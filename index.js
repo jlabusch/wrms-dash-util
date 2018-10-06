ebf720e847fa1cd50853cc9e0ffe0e6ae4d66bcb
Jacques Labuschagne <jlabusch@acm.org>
2018-10-06 14:59:56 +0100

    Adding context and org_data.

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ var log = require('./lib/log'),
     promises = require('./lib/promise_sequence');
 
 module.exports = {
-    crypt:  require('./lib/crypt'),
-    dates:  require('./lib/dates'),
-    log:        log.log,
-    log_debug:  log.log_debug,
+    context:    require('./lib/context'),
+    crypt:      require('./lib/crypt'),
+    dates:      require('./lib/dates'),
+    log:            log.log,
+    log_debug:      log.log_debug,
     map_severity:           require('./lib/map_severity'),
-    promise_sequence:       promises.promise_sequence,
+    org_data:               require('./lib/org_data'),
     ON_ERROR_CONTINUE:      promises.ON_ERROR_CONTINUE,
+    promise_sequence:       promises.promise_sequence,
     round_to_half_hour:     require('./lib/round_to_half_hour'),
     send_err_if_not_vendor: require('./lib/send_err_if_not_vendor'),
+    server:     require('./lib/server'),
     swapper:    require('./lib/swapper'),
     trim:       require('./lib/trim')
 }
