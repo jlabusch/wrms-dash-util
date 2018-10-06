49165d4b962bd6ea12de482d1555361c9cdeebbc
Jacques Labuschagne <jlabusch@acm.org>
2018-10-06 09:14:47 +0100

    Integrating with wrms-dash

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-modules.exports = {
+var log = require('./lib/log'),
+    promises = require('./lib/promise_sequence');
+
+module.exports = {
     crypt:  require('./lib/crypt'),
     dates:  require('./lib/dates'),
-    log:    require('./lib/log'),
+    log:        log.log,
+    log_debug:  log.log_debug,
     map_severity:           require('./lib/map_severity'),
-    promise_sequence:       require('./lib/promise_sequence'),
+    promise_sequence:       promises.promise_sequence,
+    ON_ERROR_CONTINUE:      promises.ON_ERROR_CONTINUE,
     round_to_half_hour:     require('./lib/round_to_half_hour'),
     send_err_if_not_vendor: require('./lib/send_err_if_not_vendor'),
     swapper:    require('./lib/swapper'),
