c8f511d55b928804bd668532ba45a38f14b534ac
Jacques Labuschagne <jlabusch@acm.org>
2018-10-06 23:06:59 +0100

    Added quote funcs.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ module.exports = {
     org_data:               require('./lib/org_data'),
     ON_ERROR_CONTINUE:      promises.ON_ERROR_CONTINUE,
     promise_sequence:       promises.promise_sequence,
+    quote_funcs:            require('./lib/quote_funcs'),
     round_to_half_hour:     require('./lib/round_to_half_hour'),
     send_err_if_not_vendor: require('./lib/send_err_if_not_vendor'),
     server:     require('./lib/server'),
